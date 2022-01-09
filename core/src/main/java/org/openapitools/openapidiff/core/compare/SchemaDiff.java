e45c53942776f3fa8c011d4e80d5f7978aee15a0
Julien Richard-Foy <julien@richard-foy.fr>
2022-01-10 00:10:39 +0100

    Fix #317

--- core/src/main/java/org/openapitools/openapidiff/core/compare/SchemaDiff.java
+++ core/src/main/java/org/openapitools/openapidiff/core/compare/SchemaDiff.java
@@ -154,6 +154,16 @@ public class SchemaDiff {
       }
       schema.getExtensions().putAll(fromSchema.getExtensions());
     }
+    if (fromSchema instanceof ComposedSchema && schema instanceof ComposedSchema) {
+      ComposedSchema composedFromSchema = (ComposedSchema) fromSchema;
+      ComposedSchema composedSchema = (ComposedSchema) schema;
+      if (composedFromSchema.getOneOf() != null) {
+        if (composedSchema.getOneOf() == null) {
+          composedSchema.setOneOf(new ArrayList<>());
+        }
+        composedSchema.getOneOf().addAll(composedFromSchema.getOneOf());
+      }
+    }
     if (fromSchema.getDiscriminator() != null) {
       if (schema.getDiscriminator() == null) {
         schema.setDiscriminator(new Discriminator());
