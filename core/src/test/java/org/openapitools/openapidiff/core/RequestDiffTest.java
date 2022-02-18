98137795206ddb1004d42511d25faf0c6aaad9b3
Benoit Brayer <brayer@alma.fr>
2022-02-18 14:02:34 +0100

    Add testSameTypeResponseSchemaDIff based on issue #192.

--- core/src/test/java/org/openapitools/openapidiff/core/RequestDiffTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/RequestDiffTest.java
@@ -1,8 +1,10 @@
 package org.openapitools.openapidiff.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.openapitools.openapidiff.core.TestUtils.assertOpenApiChangedEndpoints;
 
 import org.junit.jupiter.api.Test;
+import org.openapitools.openapidiff.core.model.ChangedOpenApi;
 
 public class RequestDiffTest {
   private final String OPENAPI_DOC1 = "request_diff_1.yaml";
@@ -11,5 +13,14 @@ public class RequestDiffTest {
   @Test
   public void testDiffDifferent() {
     assertOpenApiChangedEndpoints(OPENAPI_DOC1, OPENAPI_DOC2);
+  }
+
+  @Test
+  public void testSameTypeResponseSchemaDIff() {
+    ChangedOpenApi changedOpenApi =
+        OpenApiCompare.fromLocations(
+            "response_schema_simple_object_type.yaml",
+            "response_schema_simple_object_type_with_alloff.yaml");
+    assertThat(changedOpenApi.isCompatible()).isTrue();
   }
 }
