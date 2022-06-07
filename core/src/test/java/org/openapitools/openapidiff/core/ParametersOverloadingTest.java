c60890fe2532aa9d1c92848591c874ae7d7f8478
Jochen Schalanda <jochen@schalanda.name>
2022-06-08 00:13:27 +0200

    Format code

--- core/src/test/java/org/openapitools/openapidiff/core/ParametersOverloadingTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/ParametersOverloadingTest.java
@@ -1,26 +1,27 @@
 package org.openapitools.openapidiff.core;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.openapitools.openapidiff.core.TestUtils.assertOpenApiAreEquals;
 
+import org.junit.jupiter.api.Test;
+
 class ParametersOverloadingTest {
 
-    private final String OVERLOADED_PARAMETERS = "parameters_overloading.yaml";
-    private final String DUPLICATED_PARAMETER_TYPES = "parameters_overloading_2.yaml";
+  private final String OVERLOADED_PARAMETERS = "parameters_overloading.yaml";
+  private final String DUPLICATED_PARAMETER_TYPES = "parameters_overloading_2.yaml";
 
-    @Test
-    void testDiffWithOverloadedParameterTypes() {
-        assertDoesNotThrow(() -> OpenApiCompare.fromLocations(OVERLOADED_PARAMETERS, OVERLOADED_PARAMETERS));
-        assertOpenApiAreEquals(OVERLOADED_PARAMETERS, OVERLOADED_PARAMETERS);
-    }
+  @Test
+  void testDiffWithOverloadedParameterTypes() {
+    assertDoesNotThrow(
+        () -> OpenApiCompare.fromLocations(OVERLOADED_PARAMETERS, OVERLOADED_PARAMETERS));
+    assertOpenApiAreEquals(OVERLOADED_PARAMETERS, OVERLOADED_PARAMETERS);
+  }
 
-    @Test
-    void testDiffWithDuplicatedParameterTypes() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> OpenApiCompare.fromLocations(DUPLICATED_PARAMETER_TYPES, DUPLICATED_PARAMETER_TYPES),
-                "Two path items have the same signature: /projects/{}");
-    }
+  @Test
+  void testDiffWithDuplicatedParameterTypes() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> OpenApiCompare.fromLocations(DUPLICATED_PARAMETER_TYPES, DUPLICATED_PARAMETER_TYPES),
+        "Two path items have the same signature: /projects/{}");
+  }
 }
