651b211e58cb29f612c49c7028c1a2be529d5aaa
eacolina <eduardo.colina@instacart.com>
2022-06-22 13:53:54 -0400

    Write diff directly  to file

--- cli/src/main/java/org/openapitools/openapidiff/cli/Main.java
+++ cli/src/main/java/org/openapitools/openapidiff/cli/Main.java
@@ -196,9 +196,9 @@ public class Main {
       }
       if (line.hasOption("json")) {
         JsonRender jsonRender = new JsonRender();
-        String output = jsonRender.render(result);
         String outputFile = line.getOptionValue("json");
-        writeOutput(output, outputFile);
+        jsonRender.renderToFile(result, outputFile);
+        System.out.println("Succesfully wrote diff to output file");
       }
       if (line.hasOption("state")) {
         System.out.println(result.isChanged().getValue());
--- core/src/main/java/org/openapitools/openapidiff/core/output/JsonRender.java
+++ core/src/main/java/org/openapitools/openapidiff/core/output/JsonRender.java
@@ -3,6 +3,8 @@ package org.openapitools.openapidiff.core.output;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.file.Paths;
 import org.openapitools.openapidiff.core.model.ChangedOpenApi;
 
 public class JsonRender implements Render {
@@ -15,6 +17,16 @@ public class JsonRender implements Render {
       return objectMapper.writeValueAsString(diff);
     } catch (JsonProcessingException e) {
       throw new RuntimeException("Could not serialize diff as JSON", e);
+    }
+  }
+
+  public void renderToFile(ChangedOpenApi diff, String file) {
+    try {
+      objectMapper.writeValue(Paths.get(file).toFile(), diff);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Could not serialize diff as JSON", e);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not write to JSON file", e);
     }
   }
 }
