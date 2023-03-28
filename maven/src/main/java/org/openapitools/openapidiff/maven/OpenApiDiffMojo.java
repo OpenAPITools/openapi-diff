ae725797b0a624bd1e25c10fa72bbb587924f397
Martin Morissette <martin_morissette@outlook.com>
2023-03-28 19:40:53 -0400

    issue-501: Added fileoutput parameters on maven plugin. No tests added yet.

--- maven/src/main/java/org/openapitools/openapidiff/maven/OpenApiDiffMojo.java
+++ maven/src/main/java/org/openapitools/openapidiff/maven/OpenApiDiffMojo.java
@@ -1,5 +1,7 @@
 package org.openapitools.openapidiff.maven;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -9,6 +11,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.openapitools.openapidiff.core.OpenApiCompare;
 import org.openapitools.openapidiff.core.model.ChangedOpenApi;
 import org.openapitools.openapidiff.core.output.ConsoleRender;
+import org.openapitools.openapidiff.core.output.MarkdownRender;
 
 /** A Maven Mojo that diffs two OpenAPI specifications and reports on differences. */
 @Mojo(name = "diff", defaultPhase = LifecyclePhase.TEST)
@@ -28,6 +31,15 @@ public class OpenApiDiffMojo extends AbstractMojo {
   @Parameter(property = "skip", defaultValue = "false")
   Boolean skip = false;
 
+  @Parameter(property = "consoleOutputFileName", defaultValue = "")
+  String consoleOutputFileName;
+
+  @Parameter(property = "jsonOutputFileName", defaultValue = "")
+  String jsonOutputFileName;
+
+  @Parameter(property = "markdownOutputFileName", defaultValue = "")
+  String markdownOutputFileName;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     if (Boolean.TRUE.equals(skip)) {
@@ -37,7 +49,10 @@ public class OpenApiDiffMojo extends AbstractMojo {
 
     try {
       final ChangedOpenApi diff = OpenApiCompare.fromLocations(oldSpec, newSpec);
-      getLog().info(new ConsoleRender().render(diff));
+
+      generateConsoleOutput(diff);
+      generateJson(diff);
+      generateMarkdown(diff);
 
       if (failOnIncompatible && diff.isIncompatible()) {
         throw new BackwardIncompatibilityException("The API changes broke backward compatibility");
@@ -48,6 +63,42 @@ public class OpenApiDiffMojo extends AbstractMojo {
       }
     } catch (RuntimeException e) {
       throw new MojoExecutionException("Unexpected error", e);
+    }
+  }
+
+  private void generateJson(final ChangedOpenApi diff) {
+    if (isValidFileName(jsonOutputFileName)) {
+      final String render = new MarkdownRender().render(diff);
+      writeToFile(render, jsonOutputFileName);
+    }
+  }
+
+  private void generateConsoleOutput(final ChangedOpenApi diff) {
+    final String render = new ConsoleRender().render(diff);
+    if (isValidFileName(consoleOutputFileName)) {
+      writeToFile(render, consoleOutputFileName);
+    }
+    getLog().info(render);
+  }
+
+  private void generateMarkdown(final ChangedOpenApi diff) {
+    if (isValidFileName(markdownOutputFileName)) {
+      final String render = new MarkdownRender().render(diff);
+      writeToFile(render, markdownOutputFileName);
+    }
+  }
+
+  private boolean isValidFileName(String fileName) {
+    return fileName != null && !fileName.isEmpty();
+  }
+
+  private static void writeToFile(final String render, final String filename) {
+    try {
+      final FileWriter fileWriter = new FileWriter(filename);
+      fileWriter.write(render);
+      fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
