9f7e3f8f715c04130e982e92e65a83fe26e860d1
Martin Morissette <martin_morissette@outlook.com>
2023-03-30 21:16:42 -0400

    issue-501: Cleanup

--- core/src/test/java/org/openapitools/openapidiff/core/utils/FileUtilsTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/utils/FileUtilsTest.java
@@ -2,7 +2,6 @@ package org.openapitools.openapidiff.core.utils;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
