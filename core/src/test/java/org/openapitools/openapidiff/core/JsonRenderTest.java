baed2e4d5b543e4ec7631d11ce981c384194ca82
lsalgo <lsalgo@ohpen.com>
2023-07-18 16:27:31 +0200

    fix(543): fix renderer memory issues by writing to file

--- core/src/test/java/org/openapitools/openapidiff/core/JsonRenderTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/JsonRenderTest.java
@@ -2,6 +2,8 @@ package org.openapitools.openapidiff.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
 import org.junit.jupiter.api.Test;
 import org.openapitools.openapidiff.core.model.ChangedOpenApi;
 import org.openapitools.openapidiff.core.output.JsonRender;
@@ -10,16 +12,22 @@ public class JsonRenderTest {
   @Test
   public void renderDoesNotFailWhenPropertyHasBeenRemoved() {
     JsonRender render = new JsonRender();
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
     ChangedOpenApi diff =
         OpenApiCompare.fromLocations("missing_property_1.yaml", "missing_property_2.yaml");
-    assertThat(render.render(diff)).isNotBlank();
+    render.render(diff, outputStreamWriter);
+    assertThat(outputStream.toString()).isNotBlank();
   }
 
   @Test
   public void renderDoesNotFailForJsr310Types() {
     JsonRender render = new JsonRender();
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
     ChangedOpenApi diff =
         OpenApiCompare.fromLocations("jsr310_property_1.yaml", "jsr310_property_2.yaml");
-    assertThat(render.render(diff)).isNotBlank();
+    render.render(diff, outputStreamWriter);
+    assertThat(outputStream.toString()).isNotBlank();
   }
 }
