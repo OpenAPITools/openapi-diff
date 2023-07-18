3f43dc55b97fdf0910c84cb8ba61ecc08fdfdbfb
westse <10818305+westse@users.noreply.github.com>
2023-07-18 16:26:46 -0600

    Configurable incompatible checks - Required

--- core/src/main/java/org/openapitools/openapidiff/core/model/BackwardIncompatibleProp.java
+++ core/src/main/java/org/openapitools/openapidiff/core/model/BackwardIncompatibleProp.java
@@ -9,8 +9,10 @@ import org.apache.commons.configuration2.Configuration;
 public enum BackwardIncompatibleProp {
   REQUEST_ENUM_DECREASED("incompatible.request.enum.decreased", true),
   REQUEST_MAX_LENGTH_DECREASED("incompatible.request.max.length.decreased", true),
+  REQUEST_REQUIRED_INCREASED("incompatible.request.required.increased", true),
   RESPONSE_ENUM_INCREASED("incompatible.response.enum.increased", true),
   RESPONSE_MAX_LENGTH_INCREASED("incompatible.response.max.length.increased", true),
+  RESPONSE_REQUIRED_DECREASED("incompatible.response.required.decreased", true),
   ;
 
   private final String propertyName;
--- core/src/test/java/org/openapitools/openapidiff/core/backcompat/RequiredBCTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/backcompat/RequiredBCTest.java
@@ -1,10 +1,13 @@
 package org.openapitools.openapidiff.core.backcompat;
 
-import static org.openapitools.openapidiff.core.TestUtils.assertOpenApiBackwardIncompatible;
 import static org.openapitools.openapidiff.core.TestUtils.assertSpecChangedButCompatible;
+import static org.openapitools.openapidiff.core.TestUtils.assertSpecIncompatible;
 import static org.openapitools.openapidiff.core.TestUtils.assertSpecUnchanged;
+import static org.openapitools.openapidiff.core.model.BackwardIncompatibleProp.REQUEST_REQUIRED_INCREASED;
+import static org.openapitools.openapidiff.core.model.BackwardIncompatibleProp.RESPONSE_REQUIRED_DECREASED;
 
 import org.junit.jupiter.api.Test;
+import org.openapitools.openapidiff.core.model.BackwardIncompatibleProp;
 
 public class RequiredBCTest {
 
@@ -22,11 +25,13 @@ public class RequiredBCTest {
 
   @Test
   public void requestRequiredIncreased() {
-    assertOpenApiBackwardIncompatible(BASE, "bc_request_required_increased.yaml");
+    BackwardIncompatibleProp prop = REQUEST_REQUIRED_INCREASED;
+    assertSpecIncompatible(BASE, "bc_request_required_increased.yaml", prop);
   }
 
   @Test
   public void responseRequiredDecreased() {
-    assertOpenApiBackwardIncompatible(BASE, "bc_response_required_decreased.yaml");
+    BackwardIncompatibleProp prop = RESPONSE_REQUIRED_DECREASED;
+    assertSpecIncompatible(BASE, "bc_response_required_decreased.yaml", prop);
   }
 }
