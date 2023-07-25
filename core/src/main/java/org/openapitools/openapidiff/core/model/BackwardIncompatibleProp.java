c51495f8e380ee84ab43d65a15b3dd08a54cbba6
westse <10818305+westse@users.noreply.github.com>
2023-07-25 13:52:18 -0600

    Configurable incompatible checks - SecurityRequirement

--- core/src/main/java/org/openapitools/openapidiff/core/model/BackwardIncompatibleProp.java
+++ core/src/main/java/org/openapitools/openapidiff/core/model/BackwardIncompatibleProp.java
@@ -37,6 +37,8 @@ public enum BackwardIncompatibleProp {
   RESPONSE_RESPONSES_DECREASED("incompatible.response.responses.decreased", true),
   RESPONSE_WRITEONLY_INCREASED("incompatible.response.writeonly.increased", true),
   RESPONSE_WRITEONLY_REQUIRED_DECREASED("incompatible.response.writeonly.required.decreased", true),
+  SECURITY_REQUIREMENT_SCHEMES_INCREASED(
+      "incompatible.security.requirement.schemes.increased", true),
   SECURITY_SCHEME_BEARER_FORMAT_CHANGED("incompatible.security.scheme.bearer.format.changed", true),
   SECURITY_SCHEME_OPENIDCONNECT_URL_CHANGED(
       "incompatible.security.scheme.openidconnect.url.changed", true),
