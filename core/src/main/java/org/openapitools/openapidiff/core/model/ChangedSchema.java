209b7c6997c8be4af22708425f9f1f49432f0965
Aliaksandr Pinchuk <aliaksandr.pin4uk@gmail.com>
2024-10-30 19:28:23 +0100

    Improve change detection to consider changeDefault field in ChangedSchema

--- core/src/main/java/org/openapitools/openapidiff/core/model/ChangedSchema.java
+++ core/src/main/java/org/openapitools/openapidiff/core/model/ChangedSchema.java
@@ -136,6 +136,7 @@ public class ChangedSchema implements ComposedChanged {
     if (!changedType
         && (oldSchema == null && newSchema == null || oldSchema != null && newSchema != null)
         && !changeFormat
+        && !changeDefault
         && increasedProperties.isEmpty()
         && missingProperties.isEmpty()
         && changedProperties.values().isEmpty()
