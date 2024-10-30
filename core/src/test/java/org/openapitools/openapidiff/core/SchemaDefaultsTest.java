769f3f73f6902c64f3cf467293851f21bf761ec7
Aliaksandr Pinchuk <aliaksandr.pin4uk@gmail.com>
2024-10-30 19:58:32 +0100

    Add test for defaults handling in Schema

--- core/src/test/java/org/openapitools/openapidiff/core/SchemaDefaultsTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/SchemaDefaultsTest.java
@@ -9,18 +9,18 @@ import org.openapitools.openapidiff.core.model.ChangedSchema;
 
 public class SchemaDefaultsTest {
 
-    @Test
-    public void issue717DefaultsInSchema() {
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(
+  @Test
+  public void issue717DefaultsInSchema() {
+    ChangedOpenApi changedOpenApi =
+        OpenApiCompare.fromLocations(
             "issue-717-schema-defaults-handling-1.yaml",
-            "issue-717-schema-defaults-handling-2.yaml"
-        );
+            "issue-717-schema-defaults-handling-2.yaml");
 
-        assertEquals(1, changedOpenApi.getChangedOperations().size());
-        assertEquals(1, changedOpenApi.getChangedSchemas().size());
-        ChangedSchema changedSchema = changedOpenApi.getChangedSchemas().get(0);
-        assertEquals(1, changedSchema.getChangedProperties().size());
-        assertTrue(changedSchema.getChangedProperties().containsKey("field1"));
-        assertTrue(changedSchema.getChangedProperties().get("field1").isChangeDefault());
-    }
+    assertEquals(1, changedOpenApi.getChangedOperations().size());
+    assertEquals(1, changedOpenApi.getChangedSchemas().size());
+    ChangedSchema changedSchema = changedOpenApi.getChangedSchemas().get(0);
+    assertEquals(1, changedSchema.getChangedProperties().size());
+    assertTrue(changedSchema.getChangedProperties().containsKey("field1"));
+    assertTrue(changedSchema.getChangedProperties().get("field1").isChangeDefault());
+  }
 }
