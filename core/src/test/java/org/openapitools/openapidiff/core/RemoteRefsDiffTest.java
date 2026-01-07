34033e108e2e257c1e62ea9a7293a149aa304322
Charles Treatman <ctreatman@equinix.com>
2026-01-07 11:53:02 -0600

    add more extensive tests of behavior with external $refs

--- core/src/test/java/org/openapitools/openapidiff/core/RemoteRefsDiffTest.java
+++ core/src/test/java/org/openapitools/openapidiff/core/RemoteRefsDiffTest.java
@@ -1,21 +1,42 @@
 package org.openapitools.openapidiff.core;
 
 import static org.openapitools.openapidiff.core.TestUtils.assertOpenApiAreEquals;
+import static org.openapitools.openapidiff.core.TestUtils.assertOpenApiChangedEndpoints;
+import static org.openapitools.openapidiff.core.TestUtils.assertSpecChangedButCompatible;
 
 import org.junit.jupiter.api.Test;
 
 public class RemoteRefsDiffTest {
 
-  private final String OPENAPI_DOC1 = "remoteRefs/diff_1.yaml";
-  private final String OPENAPI_DOC2 = "remoteRefs/diff_2.yaml";
+  private final String OPENAPI_NESTED_REFS = "remoteRefs/specUsingNestedRefs.yaml";
+  private final String OPENAPI_NO_REFS = "remoteRefs/specUsingNoRefs.yaml";
+  private final String OPENAPI_SIMPLE_REFS = "remoteRefs/specUsingSimpleRefs.yaml";
+  private final String OPENAPI_ELEMENT_REFS = "remoteRefs/specUsingElementRefs.yaml";
+  private final String OPENAPI_MODIFIED_REFS = "remoteRefs/specUsingModifiedRefs.yaml";
 
   @Test
-  public void testDiffSame() {
-    assertOpenApiAreEquals(OPENAPI_DOC1, OPENAPI_DOC1);
+  public void testDiffSpecIsSameAsItself() {
+    assertOpenApiAreEquals(OPENAPI_NESTED_REFS, OPENAPI_NESTED_REFS);
   }
 
   @Test
-  public void testDiffSameWithAllOf() {
-    assertOpenApiAreEquals(OPENAPI_DOC1, OPENAPI_DOC2);
+  public void testDiffRefsSameAsInline() {
+    assertOpenApiAreEquals(OPENAPI_NESTED_REFS, OPENAPI_NO_REFS);
+  }
+
+  @Test
+  public void testDiffNestedRefsSameAsSimpleRefs() {
+    assertOpenApiAreEquals(OPENAPI_NESTED_REFS, OPENAPI_SIMPLE_REFS);
+  }
+
+  @Test
+  public void testDiffNestedRefsSameAsElementRefs() {
+    assertOpenApiAreEquals(OPENAPI_NESTED_REFS, OPENAPI_ELEMENT_REFS);
+  }
+
+  @Test
+  public void testDiffShowsChangesWhenRefContentChanges() {
+    assertOpenApiChangedEndpoints(OPENAPI_NESTED_REFS, OPENAPI_MODIFIED_REFS);
+    assertSpecChangedButCompatible(OPENAPI_NESTED_REFS, OPENAPI_MODIFIED_REFS);
   }
 }
